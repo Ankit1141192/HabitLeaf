8ed610b5ccd6c3752237caca36997685f298f29c
Ankit Kumar <ankit2914978@gmail.com>
2025-09-10 22:45:17 +0530

    frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ export default function Login({ setIsAuthenticated }) {
     const data = Object.fromEntries(formData);
 
     try {
-      const response = await fetch(`http://localhost:4000/auth/login`, {
+      const response = await fetch(`https://habitleaf.onrender.com/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
